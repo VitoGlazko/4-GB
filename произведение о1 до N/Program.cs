bad1154b9538bccf80ccdaa0860f8aa174a50f73
Glazkov Vitalii <vitalik.glazkov.2014@gmail.com>
2023-02-09 12:32:22 +0600

    Добавил варианты решения задач

---
+++ произведение о1 до N/Program.cs
@@ -0,0 +1,15 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введиет число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int total = 1;
+
+
+for (int count = 1; count <= num; count++ )
+{
+    total= total * count;
+}
+Console.WriteLine(total);
---
+++ сумма цифр/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int total = 0;
+
+
+for (int count = 1; count <= num; count++ )
+{
+    total+=count;
+}
+Console.WriteLine(total);
